dc5a69f6fa7be3f76cef873e9427becfbf30291c
Rasujon3 <rasujon3@gmail.com>
2023-01-01 03:01:44 +0600

    socket implement done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const socket = require("socket.io");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messagesRoute");
@@ -80,4 +81,26 @@ app.get("/", (req, res) => {
 
 const server = app.listen(port, () => {
   console.log("Server Started on Port", port);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    Credential: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
 });
